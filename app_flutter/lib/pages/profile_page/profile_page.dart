2726cda22565dff32e4a5b9077bdbdc3399579b3
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-11-08 15:00:50 +0530

    created name, edit phone number and email

--- app_flutter/lib/pages/profile_page/profile_page.dart
+++ app_flutter/lib/pages/profile_page/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:swiggy/constants.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -13,15 +14,65 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text(
+        title: Text(
           'MY ACCOUNT',
           style: kMyAccountTitleTextStyle,
         ),
         centerTitle: false,
-        elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: Column(),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 30,
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Saptarsi Roy'.toUpperCase(),
+                  style: kMyAccountTitleTextStyle.copyWith(
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'EDIT',
+                  style: GoogleFonts.roboto(
+                    fontSize: 12,
+                    color: kEditTextColor,
+                  ),
+                ),
+              ],
+            ),
+            
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Text(
+                  '1234567890',
+                  style: kPhoneAndEmailTextStyle,
+                ),
+                
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'abcd@email.com',
+                  style: kPhoneAndEmailTextStyle,
+                ),
+              ],
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
