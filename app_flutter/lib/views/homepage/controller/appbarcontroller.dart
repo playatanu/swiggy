06011f12d55875f7997799e67499e0135dd9a7be
Atanu Debnath <playatanuyt@gmail.com>
2021-11-08 17:31:56 +0530

    view to pages

--- app_flutter/lib/views/homepage/controller/appbarcontroller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-
-class AppbarController extends GetxController {
-  var area = 'Nabadwip'.obs;
-  var address = 'Malancha Para Amabagan'.obs;
-}
