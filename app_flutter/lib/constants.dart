15952a8141fa3894bc9ff273a7ea9e9105e71f58
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-11-08 12:17:06 +0530

    constant file containing all the constant and final variables

---
+++ app_flutter/lib/constants.dart
@@ -0,0 +1,12 @@
+//Contains constants textstyle, colours which are to be used in the code
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//TextStyle for my account title in profile page
+final kMyAccountTitleTextStyle = GoogleFonts.roboto(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+//appbar color for my account appbar
+const kMyAccountAppBarColor = Colors.white;
