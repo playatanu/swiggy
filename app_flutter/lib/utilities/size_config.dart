a4ca9000bc563887b8046c11a2593dae0b8ec555
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-11-09 13:19:27 +0530

    contains functions for getting height, width and font according to  screen sizes

---
+++ app_flutter/lib/utilities/size_config.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/widgets.dart';
+
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static late double defaultSize;
+  static late Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+    defaultSize = orientation == Orientation.landscape ? screenHeight * 0.024 : screenWidth * 0.024;
+  }
+}
+
+// Get the proportionate height as per screen size
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+  // 812 is the layout height that designer use
+  return (inputHeight / 812.0) * screenHeight;
+}
+
+// Get the proportionate height as per screen size
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+  // 375 is the layout width that designer use
+  return (inputWidth / 375.0) * screenWidth;
+}
+
+
+//Get the proportional font size as the screen size and orientation also
+double getFont(double size) {
+  double defaultsSize = SizeConfig.defaultSize * size;
+  return (defaultsSize / 10);
+}
