8f1dbdbaea49924546e52e9eb5b302746d163d96
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-11-09 13:51:56 +0530

    added textstyle for order section

--- app_flutter/lib/constants.dart
+++ app_flutter/lib/constants.dart
@@ -1,10 +1,11 @@
 //Contains constants textstyle, colours which are to be used in the code
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:swiggy/utilities/size_config.dart';
 
 //TextStyle for my account title in profile page
 final kMyAccountTitleTextStyle = GoogleFonts.roboto(
-  fontSize: 24,
+  fontSize: getFont(24),
   fontWeight: FontWeight.bold,
   color: Colors.black,
 );
@@ -14,8 +15,29 @@ const kEditTextColor = Color(0xFFFF8A00);
 
 //text style for phone number and email in profile page
 final kPhoneAndEmailTextStyle = GoogleFonts.roboto(
-  fontSize: 12,
+  fontSize: getFont(12),
   color: const Color(0xFF494747),
+);
+
+//TextStyle for resturant name in order section of profile page
+final kResturantNameTextStyle = GoogleFonts.roboto(
+  fontWeight: FontWeight.w500,
+  fontSize: getFont(14),
+  color: black,
+);
+
+//TextStyle for delivered text in order section of profile_page
+final kDeliveredTextStyle = GoogleFonts.roboto(
+  fontWeight: FontWeight.bold,
+  fontSize: getFont(14),
+  color: black,
+);
+
+//TextStyle for cost of food in order section of profile_page
+final kFoodPriceTextStyle = GoogleFonts.roboto(
+  fontWeight: FontWeight.normal,
+  fontSize: getFont(12),
+  color: const Color(0xFF7A7A7A),
 );
 
 //Color for past oder title background in profile page
